64b328f8313d3d1a28c04967a7f8eb3958be835a
KUTTY <KUTTY@MAHAVIGNESH>
2019-10-14 17:47:18 +0530

    My First commit

---
+++ Maven1/src/test/java/PP1/CC1.java
@@ -0,0 +1,33 @@
+package PP1;
+
+import org.testng.annotations.Test;
+
+public class CC1 
+{
+  @Test
+  public void mvn1()
+  {
+	  System.out.println("From mvn1");
+  }
+  @Test
+  public void mvn3()
+  {
+	  System.out.println("From mvn3");
+  }
+  @Test
+  public void mvn2()
+  {
+	  System.out.println("From mvn2");
+  }
+  @Test
+  public void mvn4()
+  {
+	  System.out.println("From mvn4");
+  }
+  @Test
+  public void mvn5()
+  {
+	  System.out.println("From mvn5");
+  }
+	
+}
